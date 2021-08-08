661fbbcb73b0588ea88c3dfa70fbee03612223d1
Shankar Gorai <68939188+Shankar-Tom@users.noreply.github.com>
2021-08-08 17:35:39 +0530

    Update accountlist.php

--- accountlist.php
+++ accountlist.php
@@ -13,15 +13,15 @@ $users = $auth->listUsers();
 <?php
 foreach ($users as $user) {
   $accountStatus=$user->disabled?'<a href="enable.php?id='.$user->uid.'"">Disabled</a>':'<a href="disbale.php?id='.$user->uid.'"">Enabled</a>';
-  echo '<tr>
-  <td>'.$user->displayName.'</td>
-  <td>'.$user->email.'</td>
-  <td>'.$user->phoneNumber.'</td>
-  <td>'.$accountStatus.'</td>
-<td><a href="editaccount.php?id='.$user->uid.'">Edit</a>
-<a href="deleteaccount.php?id='.$user->uid.'"">Delete</a></td>
+  echo "<tr>
+  <td>$user->displayName</td>
+  <td>$user->email</td>
+  <td>$user->phoneNumber</td>
+  <td>$accountStatus</td>
+<td><a href='editaccount.php?id=$user->uid'>Edit</a>
+<a href='deleteaccount.php?id=$user->uid'>Delete</a></td>
   
-</tr>';
+</tr>";
 }
 
 ?>
